f8f40d01925f8702fe3158ad0e532f66c9cbaef1
Davis Haupt <dbh937@gmail.com>
2017-05-29 14:45:51 -0400

    fixing indentation

--- backend/index.py
+++ backend/index.py
@@ -17,125 +17,137 @@ db = SQLAlchemy(app)
 from models import *
 
 def enforce_types(m):
-  if m['blue_score'] is not None:
-    try:
-      m['blue_score'] = int(m['blue_score'])
-    except ValueError:
-      m['blue_score'] = None
-  if m['red_score'] is not None:
-    try:
-      m['red_score'] = int(m['red_score'])
-    except ValueError:
-      m['red_score'] = None
-  
-  return m
+    if m['blue_score'] is not None:
+        try:
+            m['blue_score'] = int(m['blue_score'])
+        except ValueError:
+            m['blue_score'] = None
+    if m['red_score'] is not None:
+        try:
+            m['red_score'] = int(m['red_score'])
+        except ValueError:
+            m['red_score'] = None
+
+    return m
+
 
 def update_match(m, t, batch_update=False):
-  m['round_num'] = m.get('roundNum')
-  if 'roundNum' in m : del m['roundNum']
-  m['red_score'] = m.get('redscore')
-  if 'redscore' in m : del m['redscore']
-  m['blue_score'] = m.get('bluescore')
-  if 'bluescore' in m : del m['bluescore']
+    m['round_num'] = m.get('roundNum')
+    if 'roundNum' in m : del m['roundNum']
+    m['red_score'] = m.get('redscore')
+    if 'redscore' in m : del m['redscore']
+    m['blue_score'] = m.get('bluescore')
+    if 'bluescore' in m : del m['bluescore']
 
-  m = enforce_types(m)
+    m = enforce_types(m)
 
-  match = Match(tournament_id=int(t), **m)
-  q = db.session.query(Match).filter_by(tournament_id=t, round_num=m['round_num']).first()
-  if q is not None:
-    match.id = q.id
-    q = db.session.merge(match)
-  else:
-    db.session.add(match)
+    match = Match(tournament_id=int(t), **m)
+    q = db.session.query(Match).filter_by(tournament_id=t, round_num=m['round_num']).first()
+    if q is not None:
+        match.id = q.id
+        q = db.session.merge(match)
+    else:
+        db.session.add(match)
 
-  if batch_update:
-    db.session.commit()
+    if batch_update:
+        db.session.commit()
 
-  return match
+    return match
+
 
 def match_to_jsonable(t, q):
-  matches = q.filter_by(tournament=t).order_by(Match.round_num)
-  return map(lambda m: m.to_dict(), matches)
+    matches = q.filter_by(tournament=t).order_by(Match.round_num)
+    return map(lambda m: m.to_dict(), matches)
+
 
 def filled_matches(t):
-  q = db.session.query(Match)
-  for role in ['red1', 'red2', 'blue1', 'blue2']:
-    q = q.filter(getattr(Match, role).isnot(None))
-  return match_to_jsonable(t, q)
+    q = db.session.query(Match)
+    for role in ['red1', 'red2', 'blue1', 'blue2']:
+        q = q.filter(getattr(Match, role).isnot(None))
+    return match_to_jsonable(t, q)
+
 
 def completed_matches(t):
-  q = db.session.query(Match)
-  for role in ['red1', 'red2', 'blue1', 'blue2', 'red_score', 'blue_score']:
-    q = q.filter(getattr(Match, role).isnot(None))
-  return match_to_jsonable(t, q)
+    q = db.session.query(Match)
+    for role in ['red1', 'red2', 'blue1', 'blue2', 'red_score', 'blue_score']:
+        q = q.filter(getattr(Match, role).isnot(None))
+    return match_to_jsonable(t, q)
+
 
 def all_matches(t):
-  q = db.session.query(Match)
-  return match_to_jsonable(t, q)
+    q = db.session.query(Match)
+    return match_to_jsonable(t, q)
 
 
 @app.route("/api/")
 def hello():
-  return "Hello World!"
+    return "Hello World!"
+
 
 @app.route('/api/tournament', methods=['GET'])
 def list_tournaments():
-  return jsonify({'tournaments': map(lambda t: {'name': t.name, 'id': t.id, 'season': t.season}, db.session.query(Tournament).all())})
+    return jsonify({'tournaments': map(lambda t: {'name': t.name, 'id': t.id, 'season': t.season}, db.session.query(Tournament).all())})
+
 
 @app.route('/api/tournament/<t>/predict/prelims', methods=['GET'])
 def predict_tournament(t):
-  matchlist = filled_matches(db.session.query(Tournament).get(t))
+    matchlist = filled_matches(db.session.query(Tournament).get(t))
 
-  start = int(request.args.get('start', len(matchlist)*2/3))
-  scout = roboscout.scout(matchlist[:start])
-  pred = match_simulation.simulate_matchlist(matchlist, scout, start)
-  r = {p['roundNum']: {'red': p['prediction'][0], 'blue': p['prediction'][1]} for p in pred}
-  return jsonify({'data': r})
+    start = int(request.args.get('start', len(matchlist)*2/3))
+    scout = roboscout.scout(matchlist[:start])
+    pred = match_simulation.simulate_matchlist(matchlist, scout, start)
+    r = {p['roundNum']: {'red': p['prediction'][0], 'blue': p['prediction'][1]} for p in pred}
+    return jsonify({'data': r})
+
 
 @app.route('/api/tournament/<t>/predict/playoffs', methods=['GET'])
 def predict_playoffs(t):
-  matchlist = completed_matches(db.session.query(Tournament).get(t))
+    matchlist = completed_matches(db.session.query(Tournament).get(t))
 
-  alliances = json.loads(request.args.get('json', '{}'))['alliances']
-  scout = roboscout.scout(matchlist)
-  res = match_simulation.simulate_playoffs(alliances, scout)
-  return jsonify({'results': res})
+    alliances = json.loads(request.args.get('json', '{}'))['alliances']
+    scout = roboscout.scout(matchlist)
+    res = match_simulation.simulate_playoffs(alliances, scout)
+    return jsonify({'results': res})
+
 
 @app.route('/api/tournament/new', methods=['POST'])
 def create_tournament():
-  t = Tournament()
-  body = request.get_json()
-  if body is not None:
-    t.name = body.get('name')
-  db.session.add(t)
-  db.session.commit()
-  return jsonify({'id': t.id})
+    t = Tournament()
+    body = request.get_json()
+    if body is not None:
+        t.name = body.get('name')
+    db.session.add(t)
+    db.session.commit()
+    return jsonify({'id': t.id})
+
 
 @app.route('/api/tournament/<t>')
 def get_tournament(t):
-  tournament = db.session.query(Tournament).get(t)
-  r = {'name': tournament.name, 'id': tournament.id, 'matches': all_matches(tournament)}
-  return jsonify(r)
+    tournament = db.session.query(Tournament).get(t)
+    r = {'name': tournament.name, 'id': tournament.id, 'matches': all_matches(tournament)}
+    return jsonify(r)
+
 
 @app.route('/api/tournament/<t>/update-round', methods=['POST'])
 def update_round(t):
-  match = request.get_json()
-  update_match(match, t)
-  return jsonify({'result': 'success'})
+    match = request.get_json()
+    update_match(match, t)
+    return jsonify({'result': 'success'})
+
 
 @app.route('/api/tournament/<t>/update', methods=['POST'])
 def batch_update_tournament(t):
-  data = request.get_json()
-  matches = data.get('matches', [])
-  name = data.get('name', None)
-  for match in matches:
-    update_match(match, t, False)
-  if name is not None:
-    t = db.session.query(Tournament).get(t)
-    t.name = name
+    data = request.get_json()
+    matches = data.get('matches', [])
+    name = data.get('name', None)
+    for match in matches:
+        update_match(match, t, False)
+    if name is not None:
+        t = db.session.query(Tournament).get(t)
+        t.name = name
 
-  db.session.commit()
-  return jsonify({'result': 'success'})
+    db.session.commit()
+    return jsonify({'result': 'success'})
 
 if __name__ == "__main__":
-  app.run()
+    app.run()
--- backend/models.py
+++ backend/models.py
@@ -4,43 +4,45 @@ from sqlalchemy.orm import relationship
 
 from index import db
 
+
 class Match(db.Model):
-  __tablename__ = 'matches'
-  id = db.Column(db.Integer, primary_key=True)
+    __tablename__ = 'matches'
+    id = db.Column(db.Integer, primary_key=True)
 
-  round_num = db.Column(db.Integer)
+    round_num = db.Column(db.Integer)
 
-  red1 = db.Column(db.String())
-  red2 = db.Column(db.String())
-  blue1 = db.Column(db.String())
-  blue2 = db.Column(db.String())
+    red1 = db.Column(db.String())
+    red2 = db.Column(db.String())
+    blue1 = db.Column(db.String())
+    blue2 = db.Column(db.String())
 
-  red_score = db.Column(db.Integer)
-  blue_score = db.Column(db.Integer)
+    red_score = db.Column(db.Integer)
+    blue_score = db.Column(db.Integer)
 
-  tournament_id = db.Column(db.Integer, ForeignKey('tournaments.id'))
-  tournament = relationship('Tournament', backref='matches')
+    tournament_id = db.Column(db.Integer, ForeignKey('tournaments.id'))
+    tournament = relationship('Tournament', backref='matches')
 
-  def to_dict(self):
-    return {
-    'red1': self.red1,
-    'red2': self.red2,
-    'blue1': self.blue1,
-    'blue2': self.blue2,
-    'redscore': self.red_score,
-    'bluescore': self.blue_score,
-    'roundNum': self.round_num
-    }
+    def to_dict(self):
+        return {
+            'red1': self.red1,
+            'red2': self.red2,
+            'blue1': self.blue1,
+            'blue2': self.blue2,
+            'redscore': self.red_score,
+            'bluescore': self.blue_score,
+            'roundNum': self.round_num
+        }
 
-  def __repr__(self):
-    return '<Match {}>'.format(self.id)
+    def __repr__(self):
+        return '<Match {}>'.format(self.id)
+
 
 class Tournament(db.Model):
-  __tablename__ = 'tournaments'
-  id = db.Column(db.Integer, primary_key=True)
-  name = db.Column(db.String())
-  date_created = db.Column(db.DateTime, server_default=db.text('NOW()'))
-  season = db.Column(db.String(), server_default='2016-2017')
+    __tablename__ = 'tournaments'
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String())
+    date_created = db.Column(db.DateTime, server_default=db.text('NOW()'))
+    season = db.Column(db.String(), server_default='2016-2017')
 
-  def __repr__(self):
-    return '<Tournament {}>'.format(self.id)
+    def __repr__(self):
+        return '<Tournament {}>'.format(self.id)
